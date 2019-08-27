e2c2eaec0fc2903030aa3e98d91b51204eac48a6
ChaoYIN <yinchao428@hotmail.com>
2019-08-27 15:40:26 +1000

    change static folder path

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, Fragment } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
@@ -48,60 +48,62 @@ const Register = props => {
   };
 
   return (
-    <div className='form-container'>
-      <h1>
-        Account <span className='text-primary'>Register</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='name'>Name</label>
+    <Fragment>
+      <div className='form-container'>
+        <h1>
+          Account <span className='text-primary'>Register</span>
+        </h1>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              name='name'
+              value={name}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email Address</label>
+            <input
+              type='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              required
+              minLength='6'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password2'>Confirm Password</label>
+            <input
+              type='password'
+              name='password2'
+              value={password2}
+              onChange={onChange}
+              required
+              minLength='6'
+            />
+          </div>
           <input
-            type='text'
-            name='name'
-            value={name}
-            onChange={onChange}
-            required
+            type='submit'
+            value='Register'
+            className='btn btn-primary btn-block'
           />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
-          <input
-            type='email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-            minLength='6'
-          />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password2'>Confirm Password</label>
-          <input
-            type='password'
-            name='password2'
-            value={password2}
-            onChange={onChange}
-            required
-            minLength='6'
-          />
-        </div>
-        <input
-          type='submit'
-          value='Register'
-          className='btn btn-primary btn-block'
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+    </Fragment>
   );
 };
 
